7d3515099d5acd05b430c7c953f6888f2f8f54c7
savithapalanivel <53602226+savithapalanivel@users.noreply.github.com>
2019-08-12 00:00:36 -0900

    Create arr.py

---
+++ arr.py
@@ -0,0 +1,33 @@
+list=['savi','pavi','nithi']
+list.append('kavi')
+list.append('meena')
+print (list)
+list.remove('pavi')
+print(list)
+list[1]='ajay'
+print(list)
+list.remove('kavi')
+list.remove('meena')
+print(list)
+list=['savi','pavi','nithi']
+list.append('kavi')
+list.append('meena')
+print (list)
+list.remove('pavi')
+print(list)
+list[1]='ajay'
+print(list)
+list.remove('kavi')
+list.remove('meena')
+print(list)
+list=['savi','pavi','nithi']
+list.append('kavi')
+list.append('meena')
+print (list)
+list.remove('pavi')
+print(list)
+list[1]='ajay'
+print(list)
+list.remove('kavi')
+list.remove('meena')
+print(list)
