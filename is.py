4956afcbe111d71ecd6540a48edbbb34910f860b
savithapalanivel <53602226+savithapalanivel@users.noreply.github.com>
2019-08-12 00:11:22 -0900

    Create is.py

---
+++ is.py
@@ -0,0 +1,24 @@
+def newstr(str):
+    if(len(str)>3)and(str[:2]=="Is"):
+        return str
+    return "Is"+str
+def newstr(str):
+    if(len(str)>3)and(str[:2]=="Is"):
+        return str
+    return "Is"+str
+
+
+print(newstr("code"))
+print(newstr("Isready"))
+0 comments on commit 36b0b39
+
+print(newstr("code"))
+print(newstr("Is ready"))def newstr(str):
+    if(len(str)>3)and(str[:2]=="Is"):
+        return str
+    return "Is"+str
+
+
+print(newstr("code"))
+print(newstr("Isready"))
+0 comments on commit 36b0b39
